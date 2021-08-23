073f0dffd248b867c9b98db45835d98cd2693c43
Vladislav <61802001+Vlatislav@users.noreply.github.com>
2021-08-24 01:30:15 +0300

    fix bag, add history.push

--- src/pages/navbar/Navbar.tsx
+++ src/pages/navbar/Navbar.tsx
@@ -1,19 +1,21 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import './styles.scss'
 
 function Navbar(){
-
+  const history = useHistory()
   return(
     <nav>
           <ul style={{display:'flex',flexDirection: 'row'}}>
             <li>
-              <a href="/first">FirstPages</a>
+              <a onClick={()=>history.push('/first')}>FirstPages</a>
             </li>
             <li>
-              <a href="/second">SecondPages</a>
+              <a onClick={()=>history.push('/second')}>SecondPages</a>
             </li>
             <li>
-              <a href="/third">ThirdPages</a>
+              <a onClick={()=>history.push('/third')}>ThirdPages</a>
             </li>
           </ul>
     </nav>
