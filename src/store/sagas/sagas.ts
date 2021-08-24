5cae3674f133fbdae242bb67936e6bf0caef19a3
Vladislav <61802001+Vlatislav@users.noreply.github.com>
2021-08-24 18:45:32 +0300

    add async methods and saga run

--- src/store/sagas/sagas.ts
+++ src/store/sagas/sagas.ts
@@ -1,8 +1,20 @@
-//import { call, put, takeEvery } from 'redux-saga/effects'
+import { call, put, takeEvery } from 'redux-saga/effects' 
+import { increment } from '../actions/incrementAction';
+import { incrementFunc } from '../services/async_methods';
+
+function* incrementCounter(action: any) {
+  try {
+      console.log(action, 'INCREMENT')
+      const newNumber:number = yield call(incrementFunc,action.payload);
+      yield put({ type: increment.INCREMENT_SUCCESS, payload: newNumber, errorMessage: '' });
+  } catch (e) {
+      yield put({ type: increment.INCREMENT_ERROR, errorMessage: e.message });
+  }
+}
 
 
 function* sagas() {
-
+  yield takeEvery(increment.INCREMENT,incrementCounter);
 }
 
 export default sagas;
